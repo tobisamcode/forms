c3baf311b67b72f08443bed45ade8bff85f69860
tobisamcode <samueloluwatobiloba48@gmail.com>
2021-12-16 23:33:08 +0100

    changes

--- src/components/RatingControl.vue
+++ src/components/RatingControl.vue
@@ -1,23 +1,52 @@
 <template>
     <ul>
-        <li><button>Poor</button></li>
-        <li><button>Average</button></li>
-        <li><button>Great</button></li>
+        <li :class="{active: activeOption === 'poor'}"><button type="button" @click="activate('poor')">Poor</button></li>
+        <li :class="{active: activeOption === 'average'}"><button type="button" @click="activate('average')">Average</button></li>
+        <li :class="{active: activeOption === 'great'}"><button type="button" @click="activate('great')">Great</button></li>
     </ul>
 </template>
 
 
+<script>
+export default {
+    props: ['modalValue'],
+    emits: ['update:modalValue'],
+    data() {
+        return {
+            activeOption: this.modalValue
+        };
+    },
+    methods: {
+        activate(option) {
+            this.activeOption = option;
+            this.$emit('update:modalValue', option);
+        }
+    }
+}
+</script>
+
+
 <style scoped>
+.active {
+    background-color: rgb(219, 90, 219);
+}
+
+.active button{
+    color: white
+}
+
 ul {
     list-style: none;
     margin: .5rem 0;
     padding: 0;
     display: flex;
 }
+
 li {
     margin: 0 1rem;
     border: 1px solid #ccc;
     border-radius: 5px;
+    padding: 1rem;
     display: flex;
     justify-content: center;
     align-items: center;
